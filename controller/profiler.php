8dfd8e644cef3d9000041369f514611c08a1147b
Tristan Darricau <github@nicofuma.fr>
2014-09-07 00:49:39 +0200

    Fix a few issues related to export/import

--- controller/profiler.php
+++ controller/profiler.php
@@ -465,9 +465,7 @@ class profiler
 	protected function assign_admin_vars($request, $profile, $token)
 	{
 		$this->template->assign_vars(array(
-			'admin_action'	=> $this->helper->route('nicofuma_webprofiler_profiler_import'),
 			'purge_link'	=> $this->helper->route('nicofuma_webprofiler_profiler_purge', array('token' => $token)),
-			'export_link'	=> $this->helper->route('nicofuma_webprofiler_profiler_export', array('token' => $token)),
 		));
 	}
 
