6bae82ce20c8f1856770a6c30fa7e68f455cc1f7
Tristan Darricau <github@nicofuma.fr>
2014-09-06 23:25:38 +0200

    Remove export/import (see CVE-2014-6072)

--- controller/profiler.php
+++ controller/profiler.php
@@ -133,36 +133,6 @@ class profiler
 	}
 
 	/**
-	* Exports data for a given token.
-	*
-	* @param string $token The profiler token
-	*
-	* @return Response A Response instance
-	*
-	* @throws NotFoundHttpException
-	*/
-	public function exportAction($token)
-	{
-		if ($this->profiler === null)
-		{
-			throw new NotFoundHttpException('The profiler must be enabled.');
-		}
-
-		$this->profiler->disable();
-
-		$profile = $this->profiler->loadProfile($token);
-		if (!$profile)
-		{
-			throw new NotFoundHttpException(sprintf('Token "%s" does not exist.', $token));
-		}
-
-		return new Response($this->profiler->export($profile), 200, array(
-			'Content-Type' => 'text/plain',
-			'Content-Disposition' => 'attachment; filename= '.$token.'.txt',
-		));
-	}
-
-	/**
 	* Purges all tokens.
 	*
 	* @return Response A Response instance
@@ -180,40 +150,6 @@ class profiler
 		$this->profiler->purge();
 
 		return new RedirectResponse($this->helper->route('nicofuma_webprofiler_profiler_info', array('about' => 'purge')), 302, array('Content-Type' => 'text/html'));
-	}
-
-	/**
-	* Imports token data.
-	*
-	* @param Request $request The current HTTP Request
-	*
-	* @return Response A Response instance
-	*
-	* @throws NotFoundHttpException
-	*/
-	public function importAction(Request $request)
-	{
-		if ($this->profiler === null)
-		{
-			throw new NotFoundHttpException('The profiler must be enabled.');
-		}
-
-		$this->profiler->disable();
-
-		$file = $request->files->get('file');
-
-		if (empty($file) || !$file->isValid())
-		{
-			return new RedirectResponse($this->helper->route('nicofuma_webprofiler_profiler_info', array('about' => 'upload_error')), 302, array('Content-Type' => 'text/html'));
-		}
-
-		$profile = $this->profiler->import(file_get_contents($file->getPathname()));
-		if (!$profile)
-		{
-			return new RedirectResponse($this->helper->route('nicofuma_webprofiler_profiler_info', array('about' => 'already_exists')), 302, array('Content-Type' => 'text/html'));
-		}
-
-		return new RedirectResponse($this->helper->route('nicofuma_webprofiler_profiler', array('token' => $profile->getToken())), 302, array('Content-Type' => 'text/html'));
 	}
 
 	/**
